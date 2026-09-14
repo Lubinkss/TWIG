475decc4506e41ab29744a63d680b7657167a824
Le_Blanc Lubin <lubin.le-blanc9@etu.univ-lorraine.fr>
2026-09-14 12:08:31 +0200

    maison php first test

---
+++ app/src/Controller/Maison.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+
+class Maison extends AbstractController
+
+{   #[Route('/maison/{qui}')]
+    public function maison(string $qui = 'Mickey'): Response {
+        return new Response("Meuble de " . $qui);
+    }
+
+
+}
